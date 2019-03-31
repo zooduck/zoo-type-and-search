5d2d459c631e2dbff24c449169fca54b89a6919b
zooduck <zooduck@beniceoreatsomerice.com>
2019-03-31 19:38:10 +0100

    new look for demo page, fix bug with <ul> width being set incorrectly when box-sizing: border-box is used

--- src/scripts/__tests__/service/adjustWidthToCompensateForBorders.service.test.js
+++ src/scripts/__tests__/service/adjustWidthToCompensateForBorders.service.test.js
@@ -1,13 +1,28 @@
 import adjustWidthToCompensateForBorders from '../../service/adjustWidthToCompensateForBorders.service';
 
 describe('adjustWidthToCompensateForBorders', () => {
-    it('should reduce the computed width of the element by the combined horizontal width of the borders', () => {
+    it('should reduce the width of the ul element by the combined horizontal width of the borders, if the ul element does not have a box-sizing of \"border-box\"', () => {
         const  list = document.createElement('ul');
+        list.style.boxSizing = 'content-box';
         list.style.width = '150px';
         list.style.borderLeftWidth = '50px';
         list.style.borderRightWidth =  '20px';
         adjustWidthToCompensateForBorders(list);
         expect(list.style.width).toBe('80px');
+
+        list.style.width = '150px';
+        list.style.border = '0 solid coral';
+        adjustWidthToCompensateForBorders(list);
+        expect(list.style.width).toBe('150px');
+    });
+    it('should not change the width of the ul element if the ul element has a box-sizing of \"border-box\"', () => {
+        const  list = document.createElement('ul');
+        list.style.boxSizing = 'border-box';
+        list.style.width = '150px';
+        list.style.borderLeftWidth = '50px';
+        list.style.borderRightWidth =  '20px';
+        adjustWidthToCompensateForBorders(list);
+        expect(list.style.width).toBe('150px');
 
         list.style.width = '150px';
         list.style.border = '0 solid coral';
--- src/scripts/service/adjustWidthToCompensateForBorders.service.js
+++ src/scripts/service/adjustWidthToCompensateForBorders.service.js
@@ -1,9 +1,11 @@
 const adjustWidthToCompensateForBorders = (el) => {
-    const computedStyle = getComputedStyle(el);
-    const borderLeftWidth = parseFloat(computedStyle.getPropertyValue('border-left-width'));
-    const borderRightWidth = parseFloat(computedStyle.getPropertyValue('border-right-width'));
-    const width = parseFloat(computedStyle.getPropertyValue('width'));
-    el.style.width = `${width - (borderLeftWidth + borderRightWidth)}px`;
+    if (getComputedStyle(el).boxSizing !== 'border-box') {
+        const computedStyle = getComputedStyle(el);
+        const borderLeftWidth = parseFloat(computedStyle.getPropertyValue('border-left-width'));
+        const borderRightWidth = parseFloat(computedStyle.getPropertyValue('border-right-width'));
+        const width = parseFloat(computedStyle.getPropertyValue('width'));
+        el.style.width = `${width - (borderLeftWidth + borderRightWidth)}px`;
+    };
 };
 
 export default adjustWidthToCompensateForBorders;
